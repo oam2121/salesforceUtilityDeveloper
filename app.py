0ce0e4473fdbbe75ab928d2a16791f57f313b47e
Oam Shah <oamshah2121@gmail.com>
2024-11-14 14:41:29 +0530

    after Final 2

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def main():
         if st.session_state["is_authenticated"]:
              # Display the logged-in user's username
             username = st.session_state["user_data"].get("username", "Unknown User")
-            st.markdown(f"**Logged in as:** {username}")
+            st.write(f"**Logged in as:** {username}")  # Display as non-clickable plain text
             # Show options for authenticated users
             selected_section = option_menu(
                 "Sections",
